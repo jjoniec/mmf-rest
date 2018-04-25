4ac42247884ed57c790ded9cfb203756425c6aa5
Joniec, Jacek(jjoniec) <jjoniec@paypal.com>
2018-04-24 22:13:49 -0700

    Name update

--- src/test/java/pl/edu/agh/ftj/mmfrest/VariableControlerTests.java
+++ src/test/java/pl/edu/agh/ftj/mmfrest/VariableControlerTests.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
-public class VariableControlerTest {
+public class VariableControlerTests {
     @Autowired
     private WebApplicationContext context;
 
